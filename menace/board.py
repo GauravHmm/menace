687d2eebfb336c79803d54546327cbcec35da62e
GauravHmm <gauravrajpurohit2006@gmail.com>
2026-08-27 23:40:39 +0530

    winner fn

--- menace/board.py
+++ menace/board.py
@@ -15,3 +15,23 @@ class Board:
 
         self.cells[position] = self.current_player
         self.current_player = "O" if self.current_player == "X" else "X"
+
+    def winner(self):
+        winning_lines = [(0, 1, 2),
+                         (3, 4, 5),
+                         (6, 7, 8),
+                         (0, 3, 6),
+                         (1, 4, 7),
+                         (2, 5, 8),
+                         (0, 4, 8),
+                         (2, 4, 6),]
+        for x in winning_lines:
+            first=self.cells[x[0]]
+            second=self.cells[x[1]]
+            third=self.cells[x[2]]
+            if first is not None:
+                if first==second and first==third:
+                    return first
+        return
+
+    
