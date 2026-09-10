b4f3a4217edc0c34a583edb632ab21453005b3e2
GauravHmm <gauravrajpurohit2006@gmail.com>
2026-09-10 23:55:22 +0530

    add get matchbox

--- menace/menace.py
+++ menace/menace.py
@@ -1,3 +1,15 @@
+from menace.canonical import canonicalize
+from menace.matchbox import create_matchbox
+
+
 class Menace:
     def __init__(self):
         self.matchboxes = {}
+
+    def get_matchbox(self, board):
+        canonical_state, _ = canonicalize(board)
+
+        if canonical_state not in self.matchboxes:
+            self.matchboxes[canonical_state] = create_matchbox(board)
+
+        return self.matchboxes[canonical_state]
--- tests/test_menace.py
+++ tests/test_menace.py
@@ -1,5 +1,5 @@
 from menace.menace import Menace
-
+from menace.board import Board
 
 def test_menace_starts_with_no_matchboxes():
     menace = Menace()
@@ -13,3 +13,42 @@ def test_menace_can_store_matchbox():
     menace.matchboxes["---------"] = "test"
 
     assert menace.matchboxes["---------"] == "test"
+
+def test_get_matchbox_creates_matchbox():
+    menace = Menace()
+    board = Board()
+
+    matchbox = menace.get_matchbox(board)
+
+    assert matchbox.state == "---------"
+    assert matchbox.beads == {
+        0: 4,
+        1: 4,
+        4: 4
+    }
+
+def test_get_matchbox_reuses_existing_matchbox():
+    menace = Menace()
+    board = Board()
+
+    matchbox1 = menace.get_matchbox(board)
+    matchbox2 = menace.get_matchbox(board)
+
+    assert matchbox1 is matchbox2
+    assert len(menace.matchboxes) == 1
+
+def test_symmetric_boards_use_same_matchbox():
+    menace = Menace()
+
+    board1 = Board()
+    board1.make_move(0)
+
+    board2 = Board()
+    board2.make_move(2)
+
+    matchbox1 = menace.get_matchbox(board1)
+    matchbox2 = menace.get_matchbox(board2)
+
+    assert matchbox1 is matchbox2
+    assert len(menace.matchboxes) == 1
+
