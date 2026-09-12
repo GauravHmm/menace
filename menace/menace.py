a1f38b57d0b3333d5fc94999f479e19750c9d51d
GauravHmm <gauravrajpurohit2006@gmail.com>
2026-09-12 23:25:37 +0530

    Generate Box fn added

--- menace/menace.py
+++ menace/menace.py
@@ -1,6 +1,6 @@
 from menace.canonical import canonicalize
 from menace.matchbox import create_matchbox
-
+from menace.board import Board
 
 class Menace:
     def __init__(self):
@@ -13,3 +13,32 @@ class Menace:
             self.matchboxes[canonical_state] = create_matchbox(board)
 
         return self.matchboxes[canonical_state]
+    
+    def generate_matchboxes(self):
+        self._generate_matchboxes(Board())
+
+    def _generate_matchboxes(self,board):
+        if board.is_game_over():
+            return
+        if board.current_player == "X" and board.cells.count("X") < 4:
+            canonical_state,_=canonicalize(board)
+
+            if canonical_state not in self.matchboxes:
+                matchbox=create_matchbox(board)
+                self.matchboxes[canonical_state]=matchbox
+
+        for position in board.legal_moves():
+            cells = list(board.cells)
+
+            cells[position]=board.current_player
+
+            if board.current_player=="X":
+                next_player = "O"
+            else:
+                next_player = "X"
+
+            next_board=Board.from_cells(cells,next_player)
+
+            self._generate_matchboxes(next_board)
+
+
